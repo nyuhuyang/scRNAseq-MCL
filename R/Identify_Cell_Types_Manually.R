681574f6e3920eff009b8613d398ad465250cdb3
Yang Hu <nyuhuyang@gmail.com>
2019-01-03 00:39:43 -0500

    add Alias function

--- R/Identify_Cell_Types_Manually.R
+++ R/Identify_Cell_Types_Manually.R
@@ -10,46 +10,56 @@ if(!dir.exists(path)) dir.create(path, recursive = T)
 #====== 2.1 identify phenotype for each cluster  ==========================================
 (load(file="data/MCL_Harmony_20_20181231.Rda"))
 
-blueprint_encode_main = read.csv("../SingleR/output/blueprint_encode_main.csv",row.names =1,header = T,
-                                 stringsAsFactors = F)
-blueprint_encode_sub = read.csv("../SingleR/output/blueprint_encode_sub.csv",row.names =1,header = T,
-                                 stringsAsFactors = F)
+#blueprint_encode_main = read.csv("../SingleR/output/blueprint_encode_main.csv",row.names =1,header = T,
+#                                 stringsAsFactors = F)
+df_markers <- readxl::read_excel("../SingleR/output/bio-rad-markers.xlsx")
+markers = df_markers[,-grep("Alias",colnames(df_markers))]
 
-MCL_markers = read.csv("doc/MCL_markers.csv",row.names =1,header = T,
-                                stringsAsFactors = F)
+marker.list <- df2list(markers)
+marker.list %<>% lapply(function(x) HumanGenes(object,x))  %>%
+        lapply(function(x) x[1:12]) %>% lapply(function(x) x[!is.na(x)])
 
-#marker.list <- df2list(blueprint_encode_main)
-marker.list <- df2list(blueprint_encode_sub)
-marker.list <- df2list(MCL_markers)
-marker.list %<>% lapply(function(x) HumanGenes(MCL,x[1:80]))  %>%
-        lapply(function(x) x[1:9]) %>% lapply(function(x) x[!is.na(x)])
+marker.list %>% list2df %>% kable() %>% kable_styling()
 
-marker.list <- grep("B_cells", names(marker.list),
-                    value = T) %>% marker.list[.]
-marker.list %>% list2df %>% t %>% kable() %>% kable_styling()
-
-blueprint_encode_sub = read.csv("../SingleR/output/blueprint_encode_sub.csv",row.names =1,header = T,
-                                stringsAsFactors = F)
-
-FeaturePlot.1 <- function(object = MCL, x){
-        p <- FeaturePlot(object = object, 
-                         reduction.use = "tsne",
-                         features.plot = x, min.cutoff = NA, do.return =T,
-                         cols.use = c("lightgrey","blue"), pt.size = 0.5)
-        return(p)
-}
-
-dev.off()
-
-marker.list <- list("PD1" = c("PDCD1","CD274","PDCD1LG2","CTLA4"))
 for(i in 1:length(marker.list)){
-        p <- FeaturePlot.1(object = MCL, x = marker.list[[i]])
-        p1 <- do.call(plot_grid, p)
-        p1 <- p1 + ggtitle(paste(names(marker.list)[i],"markers"))+
-                theme(plot.title = element_text(hjust = 0.5,size = 20, face = "bold"))
+        p <- lapply(marker.list[[i]], function(marker) {
+                SingleFeaturePlot.1(object = object, feature = marker,pt.size = 0.5,
+                                    gradient.use = c("lightblue", "blue3"))+
+                        ggtitle(paste0(marker,Alias(gene = marker)))+
+                        theme(plot.title = element_text(hjust = 0.5,size = 15, face = "bold"))
+                })
+
         jpeg(paste0(path,names(marker.list)[i],".jpeg"),
              units="in", width=10, height=7,res=600)
-        print(p1)
-        print(paste0(i,":",length(marker.list)))
+        print(do.call(plot_grid, p)+ ggtitle(paste(names(marker.list)[i],"markers"))+
+                      theme(plot.title = element_text(hjust = 0.5,size = 20, face = "bold")))
         dev.off()
+        print(paste0(i,":",length(marker.list)))
+}
+#====== 2.2 focus on MCL heterogeneity, and T cell subsets and NK cells in the tSNE plots =====
+df_markers <- readxl::read_excel("doc/MCL-markers.xlsx")
+(markers = df_markers[,1] %>% as.matrix %>% as.character %>% HumanGenes(object,marker.genes = .))
+
+table(object@meta.data$orig.ident)
+table(object@ident)
+object@meta.data$orig.ident = gsub("BH|DJ|MD|NZ","Normal",object@meta.data$orig.ident)
+
+df_samples <- readxl::read_excel("doc/181227_scRNAseq_info.xlsx")
+colnames(df_samples) <- tolower(colnames(df_samples))
+tests <- paste0("test",c(6))
+for(test in tests){
+        sample_n = which(df_samples$tests %in% c("control",test))
+        samples <- unique(df_samples$sample[sample_n])
+        
+        cell.use <- rownames(object@meta.data)[object@meta.data$orig.ident %in% 
+                                                       c("Normal",samples)]
+        subset.object <- SubsetData(object, cells.use = cell.use)
+        subset.object@meta.data$orig.ident %>% unique %>% sort %>% print
+        
+        SplitSingleFeaturePlot(subset.object, 
+                               select.plots = c(1,3,2,4),#c(6:8,1:5)
+                               alias = df_markers, 
+                               group.by = "ident",split.by = "orig.ident",
+                               no.legend = T,label.size=3,do.print =T,
+                               markers = markers, threshold = 0.1)
 }

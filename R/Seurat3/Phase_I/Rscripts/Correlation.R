2ba263509a97915fdbd20859f0fec0fdbb7b5ea1
Yang Hu <hu.yang@nyu.edu>
2020-05-29 20:38:56 -0400

    Correlation Script

--- R/Seurat3/Phase_I/Rscripts/Correlation.R
+++ R/Seurat3/Phase_I/Rscripts/Correlation.R
@@ -15,31 +15,38 @@ args <- as.numeric(slurm_arrayid)
 print(paste0("slurm_arrayid=",args))
 
 #======1.2 load  Seurat =========================
-B_cells_MCL = readRDS(file = "data/MCL_41_B_20200225.rds")
-B_cells_MCL$orig.ident %<>% gsub("N01|N02|N03","Normal",.)
-Idents(B_cells_MCL) = "orig.ident"
-samples = c("Pt25_SB1","Pt25_1","Pt25_1_8","Pt25_24","Pt25_25Pd","Pt25_AMB25Pd",
-            "Pt10_LN2Pd","All_samples","PtB13_Ibp","PtB13_Ib1","PtB13_IbR","Normal",
-            "Pt11_LN1", "Pt17_LN1","PtU01","PtU02","PtU03","PtU04")
+sub_object = readRDS(file = "data/MCL_41_B_20200225.rds")
+sub_object$orig.ident %<>% gsub("N01|N02|N03","Normal",.)
+Idents(sub_object) = "orig.ident"
+samples <- c("All_samples","Normal","N04","PtU01","PtU02","PtU03","PtU04",
+             "Pt2_30Pd","Pt10_LN2Pd","Pt11_LN1","Pt11_1",
+             "Pt11_14","Pt11_28","Pt13_BMA1","Pt13_1a",
+             "Pt13_1b","Pt16_3Pd","Pt17_LN1","Pt17_2",
+             "Pt17_7","Pt17_12","Pt17_31","Pt19_BM2Pd",
+             "Pt20_1","Pt25_SB1","Pt25_1","Pt25_1_8",
+             "Pt25_24","Pt25_25Pd","Pt25_AMB25Pd","Pt27_1",
+             "Pt27_1_8","Pt27_12","PtB13_Ibp","PtB13_Ib1",
+             "PtB13_IbR","Pt28_LN1","Pt28_1","Pt28_4",
+             "Pt28_28")
 
 test_genes <- c("EZH2","EZH1","CCND1","E2F1","PCNA","IRF4","PIK3IP1","HLA-DPA1","MCM7",
                    "HLA-DPB1","HLA-A","HLA-B","BCL6","MYC","MEF2B","CDKN1A","NFKB2","MAP3K8",
                    "FOXM1","RELB","POLR2M","CRBN","IKZF1","IKZF3","MBOAT7")
-test_genes = test_genes[test_genes %in% rownames(B_cells_MCL)]
+test_genes = test_genes[test_genes %in% rownames(sub_object)]
 
 s = samples[args]
-sub_object <- subset(B_cells_MCL, idents = s)
+if(s != "All_samples") sub_object <- subset(sub_object, idents = s)
 ## Column clustering (adjust here distance/linkage methods to what you need!)
 sub_object <- FindVariableFeatures(object = sub_object, selection.method = "vst",
                                num.bin = 20,nfeatures = 3000,
                                mean.cutoff = c(0.1, 8), 
                                dispersion.cutoff = c(1, Inf))
-test_genes <- unique(c(VariableFeatures(sub_object),test_genes))
-y = sub_object[["SCT"]]@data[test_genes,]
+v_genes <- unique(c(VariableFeatures(sub_object),test_genes))
+y = sub_object[["SCT"]]@data[v_genes,]
 system.time(cor_res <- Hmisc::rcorr(t(as.matrix(y)), type="spearman"))
 cor_res$r[is.na(cor_res$r)] = 0
 
-save.path <- paste0(path, "Cor-",s)
+save.path <- paste0(path, s,"/")
 if(!dir.exists(save.path))dir.create(save.path, recursive = T)
 
 jpeg(paste0(save.path,"heatmap-cor-",s,".jpeg"), units="in", width=10, height=7,res=600)
@@ -57,7 +64,7 @@ for(i in seq_along(test_genes)){
         df[,"genes"] = rownames(df)
         n = 10
         low_cor <- head(df$correlation,n)[n]
-        high_cor <- tail(df$correlation,n)[n]
+        high_cor <- tail(df$correlation,n)[1]
         jpeg(paste0(save.path,"cor-pvalue-",gene,"-",s,".jpeg"), units="in", width=10, height=7,res=600)
         g <- ggline(df, x = "correlation", y = "log10.p.value",
                     numeric.x.axis = TRUE,
@@ -75,5 +82,5 @@ for(i in seq_along(test_genes)){
         Progress(i,length(test_genes))
 }
 names(df_list) = test_genes
-write.xlsx(df_list, file = paste0(path,"cor-pvalue-genes-",s,".xlsx"),
+write.xlsx(df_list, file = paste0(save.path,"cor-pvalue-summary-",s,".xlsx"),
            colNames = TRUE, borders = "surrounding",colWidths = c(NA, "auto", "auto"))

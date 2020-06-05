0692d52849151e751fa7006b3c8332ca193da8f0
Yang Hu <hu.yang@nyu.edu>
2020-06-05 08:53:07 -0400

    update SingleR dataset including GSE107011

--- R/Seurat3/Phase_I/SingleR.R
+++ R/Seurat3/Phase_I/SingleR.R
@@ -7,12 +7,13 @@ if(!dir.exists(path)) dir.create(path, recursive = T)
 #====== 3.1 Create Singler Object  ==========================================
 (load(file = "data/MCL_41_harmony_20191231.Rda"))
 object_data <- object@assays$SCT@data
-remove(object); GC()                                                             
-(load(file = 'data/ref_MCL_blue_encode_20200225.RData'))
+remove(object); GC()
+#(load(file = 'data/ref_MCL_blue_encode_20200225.RData'))
+ref = readRDS(file='data/ref_MCL_blue_encode_GSE107011_20200605.rds')
 singler <- CreateBigSingleRObject.1(object_data, annot = NULL, project.name="EC-MDL",
                                     N = 5000, min.genes = 200, technology = "10X",
                                     species = "Human", citation = "", ref.list = list(ref),
                                     normalize.gene.length = F, variable.genes = "de", fine.tune = T,
                                     reduce.file.size = F, do.signatures = F, do.main.types = T,
                                     temp.dir = getwd(), numCores = SingleR.numCores)
-save(singler,file="output/singlerT_MCL_41_20200225.Rda")
+save(singler,file="output/singlerT_MCL_41_20200605.Rda")
--- R/Seurat3/Phase_I/SingleR_reference.R
+++ R/Seurat3/Phase_I/SingleR_reference.R
@@ -6,6 +6,8 @@ library(SingleR)
 library(genefilter)
 library(dplyr)
 library(magrittr)
+library(kableExtra)
+library(GEOquery)
 source("https://raw.githubusercontent.com/nyuhuyang/SeuratExtra/master/R/Seurat3_functions.R")
 source("https://raw.githubusercontent.com/nyuhuyang/SeuratExtra/master/R/SingleR_functions.R")
 source("R/util.R")
@@ -63,17 +65,44 @@ quantile_75_new <- apply(blueprint_encode_rm,2,function(x) quantile(x,probs =0.7
 #boxplot(blueprint_encode_rm)#slow!
 #title(main="blueprint_encode_rm")
 
+# replace cell type names
+Mapvalues = data.frame(c("Preadipocytes",   "Adipocytes Pre"),
+                       c("CD4+ T-cells",   "T-cells CD4"),
+                       c("CD4+ Tcm",       "T-cells CD4 central.memory"),
+                       c("CD4+ Tem",       "T-cells CD4 effector.memory"),
+                       c("CD8+ T-cells",   "T-cells CD8"),
+                       c("CD8+ Tcm",       "T-cells CD8 central.memory"),
+                       c("CD8+ Tem",       "T-cells CD8 effector.memory"),
+                       c("Class-switched memory B-cells","B-cells class-switched.memory"),
+                       c("DC",             "Dendritic-cells myeloid"),
+                       c("Endothelial cells","Endothelial-cells"),
+                       c("Epithelial cells","Epithelial-cells"),
+                       c("Memory B-cells",  "B-cells memory"),
+                       c("Mesangial cells", "Mesangial-cells"),
+                       c("mv Endothelial cells","Endothelial-cells microvascular"),
+                       c("naive B-cells",   "B-cells naive"),
+                       c("NK cells",        "NK-cells"),
+                       c("Plasma cells",    "B-cells plasma-cells"),
+                       c("Skeletal muscle", "Skeletal-muscle"),
+                       c("Smooth muscle",   "Smooth-muscle"),
+                       c("Tregs",           "T-cells CD4 Tregs")
+                       ) %>% t %>% as.data.frame(stringsAsFactors = F)
+colnames(Mapvalues) = c("from","to")
+blue_encode_types <- data.frame("original_names" = blueprint_encode$types[-rm_index], # didn't use main_types
+                                "sub_types" = colnames(blueprint_encode_rm),
+                                stringsAsFactors = F)
 
-sort(unique(blueprint_encode$main_types))
-types = FineTune(as.character(blueprint_encode$types[-rm_index]),
-                       main.type = FALSE)
-main_types = FineTune(as.character(blueprint_encode$main_types[-rm_index]),
-                            main.type = TRUE)
-sort(unique(main_types))
+blue_encode_types$main_types = plyr::mapvalues(blue_encode_types$original_names,
+                                                  from  = Mapvalues$from, 
+                                                  to = Mapvalues$to)
+blue_encode_types$sub_types %<>% gsub("\\.[[:digit:]]+$","",.) %<>% gsub("\\.$","",.)
+# correct mistake in dataset
+blue_encode_types$main_types[blue_encode_types$sub_types == "Megakaryocytes"] = "Megakaryocytes"
+blue_encode_types %>% kable() %>% kable_styling()
 Blueprint_encode = CreateSinglerReference(name = 'Blueprint_encode',
                                           expr = blueprint_encode_rm,
-                                          types = types, 
-                                          main_types = main_types)
+                                          types = blue_encode_types$sub_types, 
+                                          main_types = blue_encode_types$main_types)
 save(Blueprint_encode,file='../SingleR/data/Blueprint_encode.RData')
 # 2. check and prepare MCL data==============================
 MCL_bulk <- read.csv(file="data/RNAseq/MCL_bulk_191006.csv", stringsAsFactors = F)
@@ -86,47 +115,106 @@ B_MCL_bulk <- RemoveDup(B_MCL_bulk)
 head(B_MCL_bulk);dim(B_MCL_bulk)
 
 B_MCL_bulk <- log1p(B_MCL_bulk)
-# 2. check and prepare MCL data==============================
-library("GEOquery")
+B_MCL_types <- data.frame("main_types" = c(rep("B-cells",ncol(B_bulk)-1),
+                                           rep("MCL-cells",ncol(MCL_bulk)-1)),
+                          "sub_types" = colnames(B_MCL_bulk),
+                          stringsAsFactors = F)
+B_MCL_types$sub_types %<>% paste(B_MCL_types$main_types, .)
 
-getGEOSuppFiles("GSE107011",baseDir = "data/RNAseq/") # get TPM, it takes some time
-data = read.table(gzfile("data/RNAseq/GSE107011/GSE107011_Processed_data_TPM.txt.gz"),
+# 2. check and prepare GSE107011 data==============================
+# get TPM, it takes some time
+getGEOSuppFiles("GSE107011",baseDir = "data/RNAseq/") 
+GSE107011_data = read.table(gzfile("data/RNAseq/GSE107011/GSE107011_Processed_data_TPM.txt.gz"),
                   header = T)
-gse <- getGEO('GSE107011') %>% .[[1]] # get cell types
-GSE107011_types <- colnames(data)
+require("biomaRt")
+mart <- useMart("ENSEMBL_MART_ENSEMBL")
+mart <- useDataset("hsapiens_gene_ensembl", mart)
+ens <- rownames(GSE107011_data)
+ensLookup <- gsub("\\.[0-9]*$", "", ens)
+annotLookup <- getBM(mart=mart,
+                     attributes=c("ensembl_gene_id","external_gene_name"),
+                     filter="ensembl_gene_id",
+                     values=ensLookup, uniqueRows=TRUE)
+annotLookup <- data.frame(ens[match(annotLookup$ensembl_gene_id, ensLookup)],
+                          annotLookup)
+colnames(annotLookup) <- c("original_id","ensembl_gene_id","external_gene_name")
+ens %<>% plyr::mapvalues(from = annotLookup$original_id, to = annotLookup$external_gene_name)
+GSE107011_data = data.frame("gene_id" = ens,
+                            GSE107011_data)
+GSE107011_data <- RemoveDup(GSE107011_data)
 
+# get cell types
+gse <- getGEO('GSE107011') %>% .[[1]] 
+GSE107011_types = gse@phenoData@data[,c("cell type:ch1","description.2")]
+colnames(GSE107011_types) = c("original_names","sub_types")
+GSE107011_types$colnames = colnames(GSE107011_data)
+GSE107011_types$sub_types %<>% as.character() %>% substring(6)
+Mapvalues = data.frame(c("Central memory CD8 T cell",  "T-cells CD8 central.memory"),
+                       c("Classical monocytes",        "Monocytes classical"),
+                       c("Effector memory CD8 T cells","T-cells CD8 effector.memory"),
+                       c("Exhausted B cells",          "B-cells exhausted"),
+                       c("Follicular helper T cells",  "T-cells follicular.helper"),
+                       c("Intermediate monocytes",     "Monocytes intermediate"),
+                       c("Low-density basophils",      "Basophils"),
+                       c("Low-density neutrophils",    "Neutrophils"),
+                       c("MAIT cells",                 "T-cells MAIT"),
+                       c("Myeloid dendritic cells",    "Dendritic-cells myeloid"),
+                       c("Naive B cells",              "B-cells naive"),
+                       c("Naive CD4 T cells",          "T-cells CD4 naive"),
+                       c("Naive CD8 T cells",          "T-cells CD8 naive"),
+                       c("Natural killer cells",       "NK-cells"),
+                       c("Non classical monocytes",    "Monocytes non.classical"),
+                       c("Non-switched memory B cells","B-cells non.switched.memory"),
+                       c("Non-Vd2 gd T cells",         "T-cells non.vd2.gd"),
+                       c("PBMCs",                      "PBMCs"),
+                       c("Plasmablasts",               "Plasmablasts"),
+                       c("Plasmacytoid dendritic cells","Dendritic-cells plasmacytoid"),
+                       c("Progenitor cells",           "Progenitor cells"),
+                       c("Switched memory B cells",    "B-cells class-switched.memory"),
+                       c("T regulatory cells",         "T-cells CD4 Tregs"),
+                       c("Terminal effector CD4 T cells","T-cells CD4 effector.terminal"),
+                       c("Terminal effector CD8 T cells","T-cells CD8 effector.terminal"),
+                       c("Th1 cells",                   "T-cells Th1"),
+                       c("Th1/Th17 cells",              "T-cells Th1.Th17"),
+                       c("Th17 cells",                  "T-cells Th17"),
+                       c("Th2 cells",                   "T-cells Th2"),
+                       c("Vd2 gd T cells",              "T-cells vd2.gd")
+                       ) %>% t %>% as.data.frame(stringsAsFactors = F)
+colnames(Mapvalues) = c("from","to")
+GSE107011_types$main_types = plyr::mapvalues(GSE107011_types$original_names,
+                                               from  = Mapvalues$from, 
+                                               to = Mapvalues$to)
 
-GSE107011_types %<>% substring(6) %<>% gsub("^_","",.)
-table(GSE107011_types)
+GSE107011_types %>% kable() %>% kable_styling()
+blue_encode_types %>% kable() %>% kable_styling()
 
-Metadata = gse@phenoData@data
 # 3. merge MCL and Blueprint_encode =====================
-(load(file='../SingleR/data/Blueprint_encode.RData'))
-dim(Blueprint_encode$data)
-MCL_blue_encode <- merge(B_MCL_bulk, Blueprint_encode$data,
+blueprint_encode_rm[1:3,1:3];B_MCL_bulk[1:3,1:3];GSE107011_data[1:3,1:3]
+
+
+temp <- merge(blueprint_encode_rm, B_MCL_bulk,
                          by="row.names",all=FALSE)
-MCL_blue_encode <- RemoveDup(MCL_blue_encode)
-testMMM(MCL_blue_encode)
+temp %<>% RemoveDup()
+blue_encode_MCL_GSE107011 <- merge(temp, log1p(GSE107011_data),
+                                   by="row.names",all=FALSE)
+blue_encode_MCL_GSE107011 %<>% RemoveDup()
 
-colsum <- colSums(MCL_blue_encode)
-#scale_factor = median(colsum)
-#MCL_blue_encode = MCL_blue_encode/colsum * scale_factor
-#testMMM(MCL_blue_encode)
+testMMM(blue_encode_MCL_GSE107011)
 
-jpeg(paste0(path,"boxplot_MCL_blue_encode.jpeg"), units="in", width=10, height=7,res=600)
+jpeg(paste0(path,"boxplot_blue_encode_MCL_GSE107011.jpeg"), units="in", width=10, height=7,res=600)
 par(mfrow=c(1,1))
-boxplot(MCL_blue_encode) #slow
-title(main = "boxplot for Blueprint + Encode + MCL")
+boxplot(blue_encode_MCL_GSE107011) #slow
+title(main = "boxplot for Blueprint + Encode + MCL+ GSE107011")
 dev.off()
 
 # Create Singler Reference
-ref = CreateSinglerReference(name = 'MCL_blue_encode',
-                             expr = as.matrix(MCL_blue_encode), # the expression matrix
-                             types = c(rep("B_cells:PB",ncol(B_bulk)-1),
-                                       rep("MCL",ncol(MCL_bulk)-1),
-                                       Blueprint_encode$types), 
-                             main_types = c(rep("B_cells",ncol(B_bulk)-1),
-                                            rep("MCL",ncol(MCL_bulk)-1),
-                                            Blueprint_encode$main_types))
+ref = CreateSinglerReference(name = 'MCL_blue_encode_GSE107011',
+                             expr = as.matrix(blue_encode_MCL_GSE107011), # the expression matrix
+                             types = c(blue_encode_types$sub_types,
+                                       B_MCL_types$sub_types ,
+                                       GSE107011_types$sub_types), 
+                             main_types = c(blue_encode_types$main_types,
+                                            B_MCL_types$main_types,
+                                            GSE107011_types$main_types))
 
-save(ref,file='data/ref_MCL_blue_encode_20200225.RData')
+saveRDS(ref,file='data/ref_MCL_blue_encode_GSE107011_20200605.rds')
